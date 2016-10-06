f454ca7210d84da64810245fee46844100ae852b
J.J. Castro <jj.castro10@uniandes.edu.co>
2016-10-06 16:46:30 -0500

    Deploy

--- deploy.php
+++ deploy.php
@@ -11,7 +11,7 @@
   $commands = array(
     'echo $PWD',
     'whoami',
-    'git pull origin master',
+    'git pull origin landing',
     'git status',
     'git submodule sync',
     'git submodule update',
